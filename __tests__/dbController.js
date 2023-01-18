54c0eefa7fade081b612e6af29685bee40d8ceeb
Yijun Liu <shirley.liu64@gmail.com>
2023-01-18 17:00:16 -0500

    saved new changes

--- __tests__/dbController.js
+++ __tests__/dbController.js
@@ -1,19 +1,43 @@
 const dbController = require('../server/controllers/dbController.js');
 
+// index:
+// 0 => makeArray
+// 1 => getFirstNames
+// 2 => getMiddleNames
+// 3 => getLastNames
+// 4 => getEmails
+// 5 => getPhoneNumbers
+// 6 => getCountry
+// 7 => getBirthday
+
+//functions
+
 describe('dbController tests', () => {
-  it('has a function makeArray', () => {
-    console.log(dbController[0]);
+  beforeAll(() => {
+    //mockreq
+    const req = {
+      query: {
+        quantity: 3,
+        birthday: true
+      }
+    };
+
+    //mockres
+    const res = {
+      locals: {
+        data: []
+      }
+    };
+  });
+
+  //if data empty, need to make obj + add properties
+  //if data has at least 1 obj in it, just add properties
+
+  it('has a function getBirthday', () => {
+    const getBirthday = dbController[7];
     console.log(dbController[0].prototype);
-    expect(dbController[0].name).toBe(makeArray);
+    expect(dbController.find()).toBe(makeArray);
     // expect(dbController.includes(makeArray)).toBe(true);
     expect(typeof dbController[0]).toBe('function');
   });
 });
-
-// describe("", () => {
-//     it("test", () => {
-//       const somethingThatReturnsAFunction = () => () => {};
-//       const theResult = somethingThatReturnsAFunction();
-//       expect(typeof theResult).toBe("function");
-//     });
-//   });
