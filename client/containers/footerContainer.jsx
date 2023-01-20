19c4eac9138d03f43058b2197f962b437d0e6c42
Tanner Malmedal <tannermalmedal@Tanners-MacBook-Pro.local>
2023-01-19 16:44:20 -0800

    fixed footer to inlude collaborators, and changed some styling on new csv/json drop down

--- client/containers/footerContainer.jsx
+++ client/containers/footerContainer.jsx
@@ -4,6 +4,8 @@ const FooterContainer = () => {
     return (
       <footer>
         <a href='https://github.com/orgs/Stab-Rabbits-AKA-Porcupines/' target="_blank"> Stab Rabbit Inc.</a>
+        <span>|</span>
+        <a href='https://github.com/Yeeti-Crabs' target="_blank"> Yeti Crabs Inc.</a>
       </footer>
     )
 }
